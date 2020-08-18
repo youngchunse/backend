0e03738e8f75042f1089c7011c673bf1f4bf5454
youngchunse <young.chun.se@gmail.com>
2020-08-18 14:50:14 -0400

    cleaned up circleci pipeline

---
+++ app/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.region
+}
+
+module "s3" {
+  source         = "./s3"
+  bucket         = var.bucket
+  acl            = var.acl
+  versioning     = var.versioning
+  lifecycle_rule = var.lifecycle_rule
+
+  dynamodb_name  = var.dynamodb_name
+  read_capacity  = var.read_capacity
+  write_capacity = var.write_capacity
+  hash_key       = var.hash_key
+  attribute_name = var.attribute_name
+  attribute_type = var.attribute_type
+}
